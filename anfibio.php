665a2168c97ba3d9a6916bb98c148bef73009195
Marcelo Diament <marcelodiament@gmail.com>
2020-05-29 22:06:30 -0300

    criando classe Anfibio, filha de AnimalVertebrado - que é filha da abstrata Animal

--- anfibio.php
+++ anfibio.php
@@ -1,6 +1,7 @@
 <?php
-require_once("./classes/AnimalVertebrado.php");
+require_once("./classes/Anfibio.php");
 $animal = new AnimalVertebrado();
+$anfibio = new Anfibio();
 require_once("./inc/head.php");
 require_once("./inc/header.php");
 ?>
@@ -8,8 +9,8 @@ require_once("./inc/header.php");
     <section class="row py-5">
         <article class="col-12 col-lg-10 mx-auto bg-light rounded p-3">
             <header class="col-12 my-3 row">
-                <h2 class="col-12"><i class="fas fa-skull"></i> Animal Vertebrado</h2>
-                <p class="col-12">Veja o que difere o Animal Vertebrado dos outros animais e as características em comum entre eles</p>
+                <h2 class="col-12"><i class="fas fa-<?= $anfibio->icon ?>"></i> CLASSE</h2>
+                <p class="col-12">Veja o que difere o Anfíbio dos outros animais vertebrados e as características em comum entre eles</p>
             </header>
             <div class="col-12">
                 <table class="table text-center">
@@ -17,6 +18,7 @@ require_once("./inc/header.php");
                         <tr>
                             <th scope="col"><i class="fas fa-tag"></i><br />Caracteríscita<br /><small>Propriedade ou método</small></th>
                             <th scope="col"><i class="fas fa-skull"></i><br />AnimalVertebrado<br /><small>Filha da classe Abstrata Animal</small></th>
+                            <th scope="col"><i class="fas fa-<?= $anfibio->icon ?>"></i><br />Anfibio<br /><small>Filha da classe AnimalVertebrado</small></th>
                             <th scope="col"><i class="fas fa-telescope"></i><br />Observações<br /><small>Detalhes sobre o acesso à propriedades ou métodos e sua visibilidade</small></th>
                         </tr>
                     </thead>
@@ -24,51 +26,60 @@ require_once("./inc/header.php");
                         <tr>
                             <td>Reino</td>
                             <td><?= $animal->getReino() ?></td>
+                            <td><?= $anfibio->getReino() ?></td>
                             <td>Propriedade <b>protegida</b> herdada da classe Abstrata e acessada através do <b>método get()</b></td>
                         </tr>
                         <tr>
                             <td>Esqueleto</td>
                             <td><?= $animal->getEsqueleto() ?></td>
+                            <td><?= $anfibio->getEsqueleto() ?></td>
                             <td>Propriedade <b>protegida</b> herdada da classe Abstrata e acessada através do <b>método get()</b></td>
                         </tr>
                         <tr>
                             <td>Músculo</td>
                             <td><?= $animal->getMusculo() ?></td>
+                            <td><?= $anfibio->getMusculo() ?></td>
                             <td>Propriedade <b>protegida</b> herdada da classe Abstrata e acessada através do <b>método get()</b></td>
                         </tr>
                         <tr>
                             <td>Pele</td>
                             <td><?= $animal->getPele() ?></td>
-                            <td>Propriedade <b>protegida</b> herdada da classe Abstrata e acessada através do <b>método get()</b></td>
+                            <td><?= $anfibio->getPele() ?></td>
+                            <td>Propriedade <b>protegida</b> herdada da classe Abstrata e acessada através do <b>método get()</b> declarado na classe Pai</td>
                         </tr>
                         <tr>
                             <td>Ambiente</td>
                             <td><?= $animal->getAmbiente() ?></td>
-                            <td>Propriedade <b>protegida</b> herdada da classe Abstrata e acessada através do <b>método get()</b></td>
+                            <td><?= $anfibio->getAmbiente() ?></td>
+                            <td>Propriedade <b>protegida</b> herdada da classe Abstrata e acessada através do <b>método get()</b> declarado na classe Pai</td>
                         </tr>
                         <tr class="bg-warning font-weight-bold">
                             <td>Especificidade</td>
                             <td>Não definido</td>
-                            <td>Propriedade <b>privada declarada e defininda na classe da espécie</b></td>
+                            <td><?= $anfibio->getEspecificidade() ?></td>
+                            <td>Propriedade <b>privada declarada e defininda na própria classe Anfibio</b></td>
                         </tr>
                         <tr>
                             <td>Respiração</td>
                             <td><?= $animal->respirar() ?></td>
-                            <td>Método <b>abstrato</b> declarado na classe abstrata Animal e definido na classe AnimalVertebrado</td>
+                            <td><?= $anfibio->respirar() ?></td>
+                            <td>Método <b>abstrato</b> de Animal, definido em AnimalVertebrado e <b>sobreposto em Anfibio</b></td>
                         </tr>
                         <tr>
                             <td>Locomoção</td>
                             <td><?= $animal->locomover() ?></td>
-                            <td>Método <b>abstrato</b> declarado na classe abstrata Animal e definido na classe AnimalVertebrado</td>
+                            <td><?= $anfibio->locomover() ?></td>
+                            <td>Método <b>abstrato</b> de Animal, definido em AnimalVertebrado e <b>sobreposto em Anfibio</b></td>
                         </tr>
                         <tr>
                             <td>Som Emitido</td>
                             <td><?= $animal->emitirSom() ?></td>
-                            <td>Método <b>abstrato</b> declarado na classe abstrata Animal e definido na classe AnimalVertebrado</td>
+                            <td><?= $anfibio->emitirSom() ?></td>
+                            <td>Método <b>abstrato</b> de Animal, definido em AnimalVertebrado e <b>sobreposto em Anfibio</b></td>
                         </tr>
                     </tbody>
                 </table>
-                <a href="./index.php" class="btn btn-dark d-block w-25 mr-auto mt-4 mb-3" id="voltarBtn"><i class="fas fa-chevron-left"></i> <b class="ml-2">Voltar</b></a>
+                <a href="./anfibio-sapo.php" class="btn btn-dark d-block w-25 ml-auto mt-4 mb-3">Conheça o Anfíbio Sapo</a>
             </div>
         </article>
     </section>
---
+++ classes/Anfibio.php
@@ -0,0 +1,39 @@
+<?php
+
+    require_once("./classes/AnimalVertebrado.php");
+
+    class Anfibio extends AnimalVertebrado {
+
+        private $especificidade;
+
+        public function __construct()
+        {
+            $this->especificidade = "Mutam a respiração ao longo do tempo";
+            $this->pele = "Pele lisa e úmida";
+            $this->ambiente = "Aquático/Terrestre";
+            $this->icon = "frog";
+        }
+
+        public function respirar() {
+            return "Brânquias/Pulmões";
+        }
+
+        public function locomover() {
+            return "Nado/Pulo";
+        }
+
+        public function emitirSom() {
+            return "Wrébit Wrébit";
+        }
+
+        public function getEspecificidade() {
+            return $this->especificidade;
+        }
+
+        public function setEspecificidade($especificidade) {
+            $this->especificidade = $especificidade;
+        }
+
+    }
+
+?>
